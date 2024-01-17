a905edb4edfe2a618adc77830f7b125c25524e76
evgeniya dudchenko <evgeniya.dudchenko.04@gmail.com>
2024-01-17 17:36:01 +0100

    add two projects

---
+++ drag-and-drop/script.js
@@ -0,0 +1,43 @@
+dragElement(document.getElementById(("mydiv")));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        var windowInnerWidth = window.innerWidth - 5;
+        var offsetLeft = elmnt.offsetLeft - pos1;
+        var offsetXXX = offsetLeft + elmnt.offsetWidth;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (offsetLeft < 1 ? 0 : offsetXXX > windowInnerWidth ? (windowInnerWidth - elmnt.offsetWidth) : offsetLeft) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
